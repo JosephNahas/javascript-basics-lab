499095d09a72c94bd3a16b51377c05345ddfa2ae
Joseph Nahas <joe_nahas@hotmail.com>
2024-09-16 17:48:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let studentName = "Joe";
+let studentAge = 32;
+let isEnrolled = true;
+
+console.log("Declare and Use Variables \n ----------------------------------");
+console.log("studentName: " + studentName);
+console.log("studentAge: " + studentAge);
+console.log("isEnrolled: " + isEnrolled + "\n");
+
+
+let favoriteGame = "Bloodborne";
+let currentYear = 2024;
+let isSpring = false;
+let favoriteSports = ["basetball", "soccer", "wrestling", "tennis"];
+const myPC = {
+    operatingSystem: "windows",
+    age: 4,
+    hasGraphicsCard: true,
+    gamesInstalled: ["hearthstone", "leagueoflegends", "baldur's gate", "wukong"],
+}
+
+
+console.log("Demonstrate Data Types \n ----------------------------------");
+console.log("favoriteGame: " + favoriteGame);
+console.log("currentYear: " + currentYear);
+console.log("isSpring: " + isSpring);
+console.log("favoriteSports: " + favoriteSports);
+console.log("myPC: \n");
+console.log(myPC);
+console.log("\n");
+
+let x = 26;
+let y = 7;
+let a = true;
+let b = false;
+
+console.log("Use Basic Operators \n ----------------------------------");
+console.log("x + y: " + (x + y));
+console.log("x - y: " + (x - y));
+console.log("x * y: " + (x * y));
+console.log("x / y: " + (x / y));
+console.log("x > y: " + (x > y));
+console.log("x < y: " + (x < y));
+console.log("x === y: " + (x === y));
+console.log("x !== y: " + (x !== y));
+console.log("a && b: " + (a && b));
+console.log("a || b: " + (a || b));
+console.log("!a: " + !a);
+
